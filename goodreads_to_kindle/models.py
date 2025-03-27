077c59f11ccc639f3e9c069c01c584876c3e6ef1
Leonardo La Rocca <leo_la_rocca@yahoo.it>
2025-03-27 17:21:57 +0100

    feat: json support for dataclasses

--- goodreads_to_kindle/models.py
+++ goodreads_to_kindle/models.py
@@ -1,30 +1,40 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+from dataclasses_json import dataclass_json
 
-@dataclass(frozen=True)
+
+@dataclass_json
+@dataclass
 class GoodReadsBook:
     authors: list[str]
-    isbn: str
-    isbn13: str
-    language: str
+    isbn: str | None
+    isbn13: str | None
+    language: str | None
     title: str
+
+    def get_file_name(self) -> str:
+        authors = ', '.join(sorted(map(lambda a: a.lower(), self.authors)))
+        file_name_parts = [self.title.lower(), authors]
+        if self.language:
+            file_name_parts.append(self.language.lower())
+        return "___".join(file_name_parts)
 
     @classmethod
     def from_scraped_item(cls, item: dict) -> GoodReadsBook:
         return cls(
             authors=item["author"],
-            isbn=item["isbn"],
-            isbn13=item["isbn13"],
-            language=item["language"],
+            isbn=item.get("isbn"),
+            isbn13=item.get("isbn13"),
+            language=item.get("language"),
             title=item["title"],
         )
     
 
-@dataclass(frozen=True)
+@dataclass_json
+@dataclass
 class User:
     goodreads_id: str
     kindle_email: str
-    supported_formats: list[str]
     books_sent_to_kindle: list[GoodReadsBook]
 
