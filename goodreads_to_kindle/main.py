50e03e528febaa213e1039778cab64f855189815
Leonardo La Rocca <leo_la_rocca@yahoo.it>
2025-03-27 17:22:59 +0100

    feat: clean main loop + add logging

--- goodreads_to_kindle/main.py
+++ goodreads_to_kindle/main.py
@@ -1,17 +1,32 @@
-from goodreads_to_kindle.goodreads_scraper.crawl import crawl
-import json
-import time
-
+from goodreads_scraper.crawl import fetch_want_to_read
+from book_provider import ZlibBookProvider
 from constants import LANG_MAP, DATA_FOLDER
+from exceptions import BookNotFoundException
 from mail import EmailManager
+from models import GoodReadsBook
 from repository import JsonRepository
 from settings import Settings
-from models import GoodReadsBook
-from exceptions import BookNotFoundException, EbookConversionError
-from utils import convert_ebook
-from book_provider import ZlibBookProvider
 
-def main():
+import logging
+
+def setup_logging(log_file: str = "main.log", level: int = logging.INFO):
+    logger = logging.getLogger()  # root logger
+    logger.setLevel(level)
+
+    formatter = logging.Formatter("[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s")
+
+    # Console handler
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    # File handler
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+
+async def main():
     settings = Settings()
 
     email_manager = EmailManager(
@@ -22,65 +37,56 @@ def main():
     )
 
     repository = JsonRepository(workdir=DATA_FOLDER)
-    book_provider =  ZlibBookProvider()
+    book_provider =  ZlibBookProvider(settings.zlib_email, settings.zlib_password)
 
-    while True:
-        for user in repository.list_users():
-            print(f"Checking user {user.goodreads_id}")
-            to_read_shelf = [GoodReadsBook.from_scraped_item(item) for item in crawl(user.goodreads_id, "to-read")]
-            missing = [book for book in to_read_shelf if book not in user.books_sent_to_kindle]
+    for user in repository.list_users():
+        print(f"Checking user {user.goodreads_id}")
+        to_read_shelf = [GoodReadsBook.from_scraped_item(item) for item in await fetch_want_to_read(user.goodreads_id)]
+        missing = [book for book in to_read_shelf if book not in user.books_sent_to_kindle]
 
-            print(f"Found {len(missing)} new books")
-            books_sent = []
-            for book in missing:
-                print(f"Finding book {book.title}")
-                book_file_to_send = None
+        print(f"Found {len(missing)} new books")
+        for book in missing:
+            print(f"Finding book {book.title}...")
 
-                # Search for the book in the repository
-                book_paths = repository.get_book_paths(book.isbn)
-                if book_paths:
-                    # Check if we have a compatible extension
-                    extensions = [path.suffix for path in book_paths]
-                    for format in user.supported_formats:
-                        if format in extensions:
-                            book_file_to_send = repository.get_book_path(book.isbn, format)
-                            print(f"Found book {book.title} in repository")
-                            break
-                    # If we don't have a compatible extension, try to convert
-                    if not book_file_to_send:
-                        try:
-                            book_file_to_send = convert_ebook(book_paths[0], user.supported_formats[0])
-                            print(f"Converted book {book.title} to {user.supported_formats[0]}")
-                        except EbookConversionError:
-                            print(f"Error converting book {book.title}")
-                            continue
-                
-                # If we don't have the book, try to download it
-                if not book_file_to_send:
-                    try:
-                        downloaded_book_file = book_provider.download_book(book, DATA_FOLDER)
-                        book_file_to_send = repository.add_book_file(downloaded_book_file, book.isbn, book_file_to_send.suffix)
-                        downloaded_book_file.unlink()
-                        print(f"Book {book.title} downloaded.")
-                    except BookNotFoundException:
-                        print(f"Book {book.title} not found")
-                        continue
+            # Search for the book in the repository
+            book_file_to_send = repository.get_book_path(book)
 
-                # Send the book
+            # If we don't have the book, try to download it
+            if not book_file_to_send:
+                try:
+                    downloaded_book_file = await book_provider.download_book(book, DATA_FOLDER)
+                    book_file_to_send = repository.add_book_file(downloaded_book_file, book)
+                    downloaded_book_file.unlink()
+                    print(f"Book {book.title} downloaded.")
+                except BookNotFoundException:
+                    print(f"Book {book.title} not found")
+                    continue
+            else:
+                print(f"Book {book.title} found in the repository")
+
+            # Send the book
+            if book_file_to_send:
                 email_manager.send_mail(
-                    send_from=settings.email_from,
                     send_to=[user.kindle_email],
                     subject=f"GoodreadsToKindle - {book.title}",
-                    text=f"This is your requested book: {book.title} by {book.author}.\n\n--\n\n",
-                    files=[str(book_file_to_send)],
+                    text=f"This is your requested book: {book.title} by {', '.join(book.authors)}.\n\n--\n\n",
+                    file_paths=[str(book_file_to_send)],
                 )
-                books_sent.append(book)
-            
-            # Update the user in the repository
-            user.books_sent_to_kindle.extend(books_sent)
-            repository.update_user(user)
-        time.sleep(100)
+                print(f"Book {book.title} sent to {user.kindle_email}")
+
+                # Update the user in the repository
+                user.books_sent_to_kindle.append(book)
+                repository.update_user(user)
+        
+        
+    
 
 
 if __name__ == "__main__":
-    main()
+    import asyncio
+    from dotenv import load_dotenv
+
+    load_dotenv()
+    setup_logging()
+
+    asyncio.run(main())
